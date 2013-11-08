b3d73e50613080bf4c68856d7e226a9f50a5e910
papplampe <papplampe@github.com>
2013-11-08 21:17:01 +0100

    hc.c - verschluesslungsmodus eingebaut

--- firmwares/analyse 3.1.1.21/hc.c
+++ firmwares/analyse 3.1.1.21/hc.c
@@ -1,42 +1,58 @@
 /*
 	$ gcc -o hc -s -O3 hc.c -lcrypto
+	$ hc cmconfig.cfg cmconfig_plain.txt d # decrypting
+	$ hc cmconfig_plain.txt cmconfig.cfg e # encrypting
 */
 #include <stdlib.h>
 #include <stdio.h>
 #include <openssl/des.h>
+	
+#define RET_GOTO(code, marker) \
+	ret = code; \
+	goto marker;
 	
 // libhtx_db.so:00059B14
 static DES_cblock hitron_key = {
 	0x57, 0x8A, 0x95, 0x8E, 0x3D, 0xD9, 0x33, 0xFC
 };
 
-void hitron_crypt(FILE *out, FILE *in)
+void hitron_crypt(FILE *out, FILE *in, int enc)
 {
 	DES_cblock block;
 	DES_key_schedule schedule;
 	DES_set_key_unchecked(&hitron_key, &schedule);
 	while(fread((void *)&block, sizeof(block), 1, in) > 0) {
-		DES_ecb_encrypt(&block, &block, &schedule, 0);
+		DES_ecb_encrypt(&block, &block, &schedule, enc);
 		fwrite((void *)&block, sizeof(block), 1, out);
 	}
 }
 
 int main(int argc, char **argv)
 {
-	FILE *in = NULL;
+	int ret = EXIT_SUCCESS;
+	FILE *in = NULL, *out = NULL;
 
-	if(argc != 2) {
-		printf("Usage: hc cmconfig.cfg");
+	if(argc != 4) {
+		printf("Usage: hc in out d|e");
 		return EXIT_FAILURE;
 	}
-	
+		
 	in = fopen(argv[1], "rb");
 	if(in == NULL) {
 		printf("could not open file\n");
 		return EXIT_FAILURE;
 	}
-	hitron_crypt(stdout, in);
+	
+	out = fopen(argv[2], "wb");
+	if(out == NULL) {
+		printf("could not open file\n");
+		RET_GOTO(EXIT_FAILURE, close_in);
+	}
+	hitron_crypt(out, in, argv[3][0] == 'e');
+	
+	fclose(out);
+close_in:
 	fclose(in);
-
+end:
 	return EXIT_SUCCESS;
 }
